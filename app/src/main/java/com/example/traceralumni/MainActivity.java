fa76410b6a8f338c5d30ee4c01b75a6ab91590af
Aldy <rizaldy.ilham@gmail.com>
2019-06-26 11:04:45 +0700

    nav bar jadi

--- app/src/main/java/com/example/traceralumni/MainActivity.java
+++ app/src/main/java/com/example/traceralumni/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.traceralumni;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout cl_icon1, cl_icon2, cl_icon3, cl_icon4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cl_icon1 = findViewById(R.id.cl_icon1);
+        cl_icon2 = findViewById(R.id.cl_icon2);
+        cl_icon3 = findViewById(R.id.cl_icon3);
+        cl_icon4 = findViewById(R.id.cl_icon4);
+
+//        cl_icon1.setVisibility(View.GONE);
+        cl_icon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "icon1 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cl_icon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "icon2 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cl_icon3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "icon3 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cl_icon4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "icon4 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
