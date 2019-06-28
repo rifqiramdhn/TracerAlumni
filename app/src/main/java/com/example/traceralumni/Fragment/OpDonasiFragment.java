07780ecce185e9dca730d31e5dab7f3fdf5a0399
Aldy <rizaldy.ilham@gmail.com>
2019-06-28 15:41:08 +0700

    rename udah

--- app/src/main/java/com/example/traceralumni/Fragment/OpDonasiFragment.java
+++ app/src/main/java/com/example/traceralumni/Fragment/OpDonasiFragment.java
@@ -24,7 +24,7 @@ public class OpDonasiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operator_donasi, container, false);
+        return inflater.inflate(R.layout.fragment_op_donasi, container, false);
     }
 
 }
--- app/src/main/java/com/example/traceralumni/Fragment/PimDataAlumniFragment.java
+++ app/src/main/java/com/example/traceralumni/Fragment/PimDataAlumniFragment.java
@@ -24,7 +24,7 @@ public class PimDataAlumniFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pimpinan_data_alumni, container, false);
+        return inflater.inflate(R.layout.fragment_pim_data_alumni, container, false);
     }
 
 }
