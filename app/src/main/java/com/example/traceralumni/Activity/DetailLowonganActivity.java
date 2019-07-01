b59e382ccc262be630d1c2c96645b0c0df1208d5
Aldy <rizaldy.ilham@gmail.com>
2019-07-01 11:44:28 +0700

    tab layout beres

--- app/src/main/java/com/example/traceralumni/Activity/DetailLowonganActivity.java
+++ app/src/main/java/com/example/traceralumni/Activity/DetailLowonganActivity.java
@@ -3,6 +3,7 @@ package com.example.traceralumni.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.traceralumni.R;
 
@@ -11,5 +12,7 @@ public class DetailLowonganActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_lowongan);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
