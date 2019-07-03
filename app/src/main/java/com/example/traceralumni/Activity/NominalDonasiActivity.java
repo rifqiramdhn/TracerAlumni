57cb6eb16de3e676cddae3259cb0a478c07d8479
Muhammad Rifqi Ramdhani <rifqiramdhn@gmail.com>
2019-07-03 11:21:08 +0700

    membagusi radio button pilih nominal donasi

--- app/src/main/java/com/example/traceralumni/Activity/NominalDonasiActivity.java
+++ app/src/main/java/com/example/traceralumni/Activity/NominalDonasiActivity.java
@@ -1,5 +1,6 @@
 package com.example.traceralumni.Activity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
@@ -7,7 +8,11 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.traceralumni.R;
@@ -18,6 +23,8 @@ public class NominalDonasiActivity extends AppCompatActivity {
     ConstraintLayout cl_iconBack, cl_iconConfirm;
     ImageView img_iconBack, img_iconConfirm;
     TextView tv_navbar;
+    EditText edt_lainnya;
+    RadioGroup rdGroup;
     AlertDialog.Builder builder;
     Intent intent;
     String namaKegiatan, totalBiaya, keterangan, fotoResId;
@@ -28,6 +35,8 @@ public class NominalDonasiActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nominal_donasi);
 
         builder = new AlertDialog.Builder(this);
+        rdGroup = findViewById(R.id.rdGroup);
+        edt_lainnya = findViewById(R.id.edt_lainnya);
 
         getData();
 
@@ -59,6 +68,48 @@ public class NominalDonasiActivity extends AppCompatActivity {
                 showKonfirmasiDialog();
             }
         });
+
+        edt_lainnya.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    rdGroup.clearCheck();
+                    edt_lainnya.requestFocus();
+                }
+            }
+        });
+
+        rdGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rdBtn_1:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_2:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_3:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_4:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_5:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_6:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_7:
+                        edt_lainnya.clearFocus();
+                        break;
+                    case R.id.rdBtn_8:
+                        edt_lainnya.clearFocus();
+                        break;
+                }
+            }
+        });
     }
 
     private void getData() {
@@ -71,7 +122,6 @@ public class NominalDonasiActivity extends AppCompatActivity {
 
     private void showKonfirmasiDialog(){
         builder.setMessage("Apakah nominal donasi yang anda masukkan sudah benar?");
-
         builder.setTitle("Konfirmasi Donasi");
         builder.setCancelable(false);
         builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
