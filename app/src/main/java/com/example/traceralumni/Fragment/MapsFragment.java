7b18b77e6e72cbc3ebde40dd2690b6b6945736fc
Aldy <rizaldy.ilham@gmail.com>
2019-07-03 16:06:42 +0700

    Maps sudah keren

--- app/src/main/java/com/example/traceralumni/Fragment/MapsFragment.java
+++ app/src/main/java/com/example/traceralumni/Fragment/MapsFragment.java
@@ -3,9 +3,12 @@ package com.example.traceralumni.Fragment;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +19,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.traceralumni.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,6 +28,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,6 +41,8 @@ import com.google.android.gms.maps.model.LatLng;
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     GoogleMap mMap;
+
+    LatLng user_touch;
 
     public MapsFragment() {
         // Required empty public constructor
@@ -74,6 +86,64 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                     .zoom(17)
                     .build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(getAddress(latLng.latitude, latLng.longitude));
+
+                user_touch = latLng;
+                mMap.clear();
+
+                Marker marker = mMap.addMarker(markerOptions);
+                marker.showInfoWindow();
+
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(latLng.latitude, latLng.longitude))
+                        .zoom(mMap.getCameraPosition().zoom)
+                        .build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+    }
+
+    public String getAddress(double lat, double lng) {
+
+//        new AsyncTask<URL, Integer, Long>() {
+//            protected Long doInBackground(URL... urls) {
+//                int count = urls.length;
+//                long totalSize = 0;
+//                for (int i = 0; i < count; i++) {
+//                    totalSize += Downloader.downloadFile(urls[i]);
+//                    publishProgress((int) ((i / (float) count) * 100));
+//                    // Escape early if cancel() is called
+//                    if (isCancelled()) break;
+//                }
+//                return totalSize;
+//            }
+//
+//            protected void onProgressUpdate(Integer... progress) {
+//                setProgressPercent(progress[0]);
+//            }
+//
+//            protected void onPostExecute(Long result) {
+//                showDialog("Downloaded " + result + " bytes");
+//            }
+//        }.execute(url1, url2, url3);
+
+        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0);
+            return add;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 }
