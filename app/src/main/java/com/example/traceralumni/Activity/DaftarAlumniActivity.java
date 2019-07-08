b3d50f37f378950c3f649535a3dd178f6c5b0d47
Muhammad Rifqi Ramdhani <rifqiramdhn@gmail.com>
2019-07-08 13:51:38 +0700

    tampilan bagian pimpinan (lihat daftar + detailnya) insyaAllah sudah selesai

---
+++ app/src/main/java/com/example/traceralumni/Activity/DaftarAlumniActivity.java
@@ -0,0 +1,127 @@
+package com.example.traceralumni.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.traceralumni.Adapter.DaftarAdapter;
+import com.example.traceralumni.Model.DaftarModel;
+import com.example.traceralumni.R;
+
+import java.util.ArrayList;
+
+import static com.example.traceralumni.Fragment.DaftarFragment.SEARCH_DAFTAR_USE_NAMA;
+import static com.example.traceralumni.Fragment.DaftarFragment.TEXT_SEARCH_DAFTAR_USE_NAMA;
+
+public class DaftarAlumniActivity extends AppCompatActivity {
+    ConstraintLayout cl_iconBack, cl_iconSearch, cl_search_daftar;
+    ImageView img_iconBack, img_iconSearch;
+    TextView tvNavBar;
+    RecyclerView daftarRecycler;
+    DaftarAdapter daftarAdapter;
+    ArrayList<DaftarModel> daftarModels;
+    EditText edt_search;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pim_daftar_alumni);
+
+        ambilView();
+        setNavBar();
+
+        daftarModels = new ArrayList<>();
+        daftarModels.add(new DaftarModel("Rifqi Ramdhani", "2019", 1));
+        daftarModels.add(new DaftarModel("Budi Fauzan", "2016", 2));
+        daftarModels.add(new DaftarModel("Rizaldy Ilham Akbar", "2014", 3));
+        daftarModels.add(new DaftarModel("Rosa Mardiana", "2014", 4));
+        daftarModels.add(new DaftarModel("Rifqi Ramdhani", "2019", 1));
+        daftarModels.add(new DaftarModel("Budi Fauzan", "2016", 2));
+        daftarModels.add(new DaftarModel("Rizaldy Ilham Akbar", "2014", 3));
+        daftarModels.add(new DaftarModel("Rosa Mardiana", "2014", 4));
+        daftarModels.add(new DaftarModel("Rifqi Ramdhani", "2019", 1));
+        daftarModels.add(new DaftarModel("Budi Fauzan", "2016", 2));
+        daftarModels.add(new DaftarModel("Rizaldy Ilham Akbar", "2014", 3));
+        daftarModels.add(new DaftarModel("Rosa Mardiana", "2014", 4));
+        daftarModels.add(new DaftarModel("Rifqi Ramdhani", "2019", 1));
+        daftarModels.add(new DaftarModel("Budi Fauzan", "2016", 2));
+        daftarModels.add(new DaftarModel("Rizaldy Ilham Akbar", "2014", 3));
+        daftarModels.add(new DaftarModel("Rosa Mardiana", "2014", 4));
+
+        daftarRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        daftarAdapter = new DaftarAdapter(this, daftarModels);
+        daftarRecycler.setAdapter(daftarAdapter);
+
+        cl_iconSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cl_search_daftar.getVisibility() == View.GONE){
+                    cl_search_daftar.setVisibility(View.VISIBLE);
+                } else {
+                    cl_search_daftar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        cl_iconBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void ambilView(){
+        cl_iconBack = findViewById(R.id.cl_icon1);
+        cl_iconSearch = findViewById(R.id.cl_icon4);
+        cl_search_daftar = findViewById(R.id.cl_activity_pim_daftar_search);
+        img_iconBack = findViewById(R.id.img_icon1);
+        img_iconSearch = findViewById(R.id.img_icon4);
+        tvNavBar = findViewById(R.id.tv_navbar_top);
+        daftarRecycler = findViewById(R.id.rv_daftar_alumni);
+        edt_search = findViewById(R.id.edt_activity_pim_daftar_search);
+    }
+
+    private void setNavBar(){
+        img_iconBack.setImageResource(R.drawable.ic_arrow_back);
+        img_iconSearch.setImageResource(R.drawable.ic_search);
+
+        cl_iconBack.setVisibility(View.VISIBLE);
+        cl_iconSearch.setVisibility(View.VISIBLE);
+
+        tvNavBar.setText("DAFTAR ALUMNI");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        edt_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                SEARCH_DAFTAR_USE_NAMA = true;
+                TEXT_SEARCH_DAFTAR_USE_NAMA = charSequence.toString().toLowerCase().trim();
+                daftarAdapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/traceralumni/Fragment/PimDataAlumniFragment.java
+++ app/src/main/java/com/example/traceralumni/Fragment/PimDataAlumniFragment.java
@@ -1,19 +1,36 @@
 package com.example.traceralumni.Fragment;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.traceralumni.Activity.DaftarAlumniActivity;
 import com.example.traceralumni.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PimDataAlumniFragment extends Fragment {
-
+    View rootView;
+    Spinner spn_jurusan, spn_prodi;
+    TextView tv_totalAlumni;
+    EditText edt_angkatan, edt_jabatan;
+    Button btn_lihatDaftar;
 
     public PimDataAlumniFragment() {
         // Required empty public constructor
@@ -23,8 +40,107 @@ public class PimDataAlumniFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pim_data_alumni, container, false);
+        rootView = inflater.inflate(R.layout.fragment_pim_data_alumni, container, false);
+
+        ambilView();
+        customSpinner();
+
+        btn_lihatDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), DaftarAlumniActivity.class);
+                startActivity(i);
+            }
+        });
+        return rootView;
     }
 
+    private void ambilView(){
+        spn_jurusan = rootView.findViewById(R.id.spn_daftar_jurusan);
+        spn_prodi = rootView.findViewById(R.id.spn_daftar_prodi);
+        tv_totalAlumni = rootView.findViewById(R.id.tv_total_alumni);
+        edt_angkatan = rootView.findViewById(R.id.edt_angkatan);
+        edt_jabatan = rootView.findViewById(R.id.edt_jabatan);
+        btn_lihatDaftar = rootView.findViewById(R.id.btn_lihat_daftar);
+    }
+
+    private void customSpinner(){
+        String[] jurusan = new String[]{
+            "Ilmu Ekonomi",
+            "Akuntansi",
+            "Manajemen"
+        };
+
+        String[] prodi = new String[]{
+            "S1 - Akuntansi (Internasional)",
+            "S1 - Ekonomi, Keuangan, dan Perbankan",
+            "S1 - Ekonomi, Keuangan, dan Perbankan (Internasional)",
+            "S1 - Ekonomi Pembangunan",
+            "S1 - Ekonomi Pembangunan (Internasional)",
+            "S1 - Kewirausahaan",
+            "S1 - Manajemen",
+            "S1 - Manajemen (Internasional)",
+            "S2 - Akuntansi",
+            "S2 - Manajemen",
+            "S2 - Ilmu Ekonomi",
+            "S3 - Ilmu Akuntansi",
+            "S3 - Ilmu Ekonomi",
+            "S3 - Ilmu Manajemen",
+            "PPAk"
+        };
+
+        final List<String> jurusanList = new ArrayList<>(Arrays.asList(jurusan));
+        final List<String> prodiList = new ArrayList<>(Arrays.asList(prodi));
+
+        final ArrayAdapter<String> spinnerArrayAdapterJurusan = new ArrayAdapter<String>(
+            rootView.getContext(), R.layout.card_spinner, jurusanList) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            public View getDropDownView(int position, View convertView, ViewGroup parent){
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0){
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(getResources().getColor(R.color.colorIconBiru));
+                }
+                return view;
+            }
+        };
+
+        final ArrayAdapter<String> spinnerArrayAdapterProdi = new ArrayAdapter<String>(
+                rootView.getContext(), R.layout.card_spinner, prodiList) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            public View getDropDownView(int position, View convertView, ViewGroup parent){
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0){
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(getResources().getColor(R.color.colorIconBiru));
+                }
+                return view;
+            }
+        };
+
+        spinnerArrayAdapterJurusan.setDropDownViewResource(R.layout.card_spinner);
+        spinnerArrayAdapterProdi.setDropDownViewResource(R.layout.card_spinner);
+        spn_jurusan.setAdapter(spinnerArrayAdapterJurusan);
+        spn_prodi.setAdapter(spinnerArrayAdapterProdi);
+    }
 }
