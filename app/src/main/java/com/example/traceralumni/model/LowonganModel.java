f3802798b0bd964d489692b7bb2cf9a8ba261ccb
Muhammad Rifqi Ramdhani <rifqiramdhn@gmail.com>
2019-07-11 12:40:48 +0700

    donasi di operator sudah bisa

--- app/src/main/java/com/example/traceralumni/model/LowonganModel.java
+++ app/src/main/java/com/example/traceralumni/model/LowonganModel.java
@@ -1,127 +1,120 @@
 package com.example.traceralumni.Model;
 
-public class LowonganModel {
-    private String username, url_logo, nama_perusahaan, nama_lowongan, lokasi_perusahaan, kisaran_gaji, syarat, jabatan, website, email, notelp, kontak;
-    private int kuota, id;
+import com.google.gson.annotations.SerializedName;
 
-    public LowonganModel(int id, String username, String url_logo, String nama_perusahaan, String nama_lowongan, String lokasi_perusahaan, String kisaran_gaji, String syarat, String jabatan, String website, String email, String notelp, String kontak, int kuota) {
-        this.id = id;
-        this.username = username;
-        this.url_logo = url_logo;
-        this.nama_perusahaan = nama_perusahaan;
-        this.nama_lowongan = nama_lowongan;
-        this.lokasi_perusahaan = lokasi_perusahaan;
-        this.kisaran_gaji = kisaran_gaji;
-        this.syarat = syarat;
-        this.jabatan = jabatan;
-        this.website = website;
-        this.email = email;
-        this.notelp = notelp;
-        this.kontak = kontak;
-        this.kuota = kuota;
-    }
+public class LowonganModel {
+    @SerializedName("id_lowongan")
+    private Integer id;
+
+    @SerializedName("username")
+    private String username;
+
+    @SerializedName("nama_lowongan")
+    private String nama_lowongan;
+
+    @SerializedName("nama_perusahaan")
+    private String nama_perusahaan;
+
+    @SerializedName("alamat_perusahaan")
+    private String alamat_perusahaan;
+
+    @SerializedName("kisaran_gaji")
+    private String kisaran_gaji;
+
+    @SerializedName("syarat_pekerjaan")
+    private String syarat_pekerjaan;
+
+    @SerializedName("pelamar_yang_dibutuhkan")
+    private Integer kuota;
+
+    @SerializedName("lowongan_jabatan")
+    private String jabatan;
+
+    @SerializedName("website_perusahaan")
+    private String website;
+
+    @SerializedName("email_perusahaan")
+    private String email;
+
+    @SerializedName("no_telp_perusahaan")
+    private String no_telp;
+
+    @SerializedName("contact_person")
+    private String cp;
+
+    @SerializedName("status")
+    private String status_lowongan;
+
+    @SerializedName("logo_perusahaan")
+    private String url_logo;
+
+
 
     public LowonganModel(int id, String nama_lowongan, String nama_perusahaan, String lokasi_perusahaan, String kisaran_gaji){
         this.id = id;
         this.nama_lowongan = nama_lowongan;
         this.nama_perusahaan = nama_perusahaan;
-        this.lokasi_perusahaan = lokasi_perusahaan;
+        this.alamat_perusahaan = lokasi_perusahaan;
         this.kisaran_gaji = kisaran_gaji;
     }
 
-    public String getUrl_logo() {
-        return url_logo;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUrl_logo(String url_logo) {
-        this.url_logo = url_logo;
-    }
-
-    public String getNama_perusahaan() {
-        return nama_perusahaan;
-    }
-
-    public void setNama_perusahaan(String nama_perusahaan) {
-        this.nama_perusahaan = nama_perusahaan;
+    public String getUsername() {
+        return username;
     }
 
     public String getNama_lowongan() {
         return nama_lowongan;
     }
 
-    public void setNama_lowongan(String nama_lowongan) {
-        this.nama_lowongan = nama_lowongan;
+    public String getNama_perusahaan() {
+        return nama_perusahaan;
     }
 
-    public String getLokasi_perusahaan() {
-        return lokasi_perusahaan;
-    }
-
-    public void setLokasi_perusahaan(String lokasi_perusahaan) {
-        this.lokasi_perusahaan = lokasi_perusahaan;
+    public String getAlamat_perusahaan() {
+        return alamat_perusahaan;
     }
 
     public String getKisaran_gaji() {
         return kisaran_gaji;
     }
 
-    public void setKisaran_gaji(String kisaran_gaji) {
-        this.kisaran_gaji = kisaran_gaji;
+    public String getSyarat_pekerjaan() {
+        return syarat_pekerjaan;
     }
 
-    public String getSyarat() {
-        return syarat;
-    }
-
-    public void setSyarat(String syarat) {
-        this.syarat = syarat;
+    public Integer getKuota() {
+        return kuota;
     }
 
     public String getJabatan() {
         return jabatan;
     }
 
-    public void setJabatan(String jabatan) {
-        this.jabatan = jabatan;
-    }
-
     public String getWebsite() {
         return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getNo_telp() {
+        return no_telp;
     }
 
-    public String getNotelp() {
-        return notelp;
+    public String getCp() {
+        return cp;
     }
 
-    public void setNotelp(String notelp) {
-        this.notelp = notelp;
+    public String getStatus_lowongan() {
+        return status_lowongan;
     }
 
-    public String getKontak() {
-        return kontak;
-    }
-
-    public void setKontak(String kontak) {
-        this.kontak = kontak;
-    }
-
-    public int getKuota() {
-        return kuota;
-    }
-
-    public void setKuota(int kuota) {
-        this.kuota = kuota;
+    public String getUrl_logo() {
+        return url_logo;
     }
 }
