a270ff2cb06663ab3b0127ac1e94f4f9475797a8
Aldy <rizaldy.ilham@gmail.com>
2019-07-25 11:04:59 +0700

    ganti nigga dan email

--- app/src/main/java/com/example/traceralumni/Activity/BantuanActivity.java
+++ app/src/main/java/com/example/traceralumni/Activity/BantuanActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,13 +20,13 @@ public class BantuanActivity extends AppCompatActivity {
     TextView tvNavBar;
     ConstraintLayout cl_iconBack;
     ImageView img_iconBack;
-    ConstraintLayout wa, telp, email;
+    ConstraintLayout wa, telp, email, wa2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bantuan);
-
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         tvNavBar = findViewById(R.id.tv_navbar_top);
         tvNavBar.setText("HUBUNGI KAMI");
         img_iconBack = findViewById(R.id.img_icon1);
@@ -50,6 +51,16 @@ public class BantuanActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        wa2 = findViewById(R.id.cl_bantuan_whatsapp2);
+        wa2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                String urlNew = "http://wa.me/6281333444861";
+                intent.setData(Uri.parse(urlNew));
+                startActivity(intent);
+            }
+        });
         telp = findViewById(R.id.cl_bantuan_telp);
         telp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,16 +77,31 @@ public class BantuanActivity extends AppCompatActivity {
             }
         });
         email = findViewById(R.id.cl_bantuan_email);
+        final String kirim [] = {"feb@ub.ac.id"};
         email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("message/rfc822");
-                intent.putExtra(Intent.EXTRA_EMAIL, "feb@ub.ac.id");
+                intent.putExtra(Intent.EXTRA_EMAIL, kirim);
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Layanan bantuan TracerAlumni");
                 intent.putExtra(Intent.EXTRA_TEXT, "");
                 startActivity(Intent.createChooser(intent, "Pilih klien email"));
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                    callIntent.setData(Uri.parse("tel:0341555000"));
+                    startActivity(callIntent);
+                }
+                return;
+            }
+        }
     }
 }
